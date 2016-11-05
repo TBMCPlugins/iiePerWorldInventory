0c72166509766a6e0ba0093cf9cd538e6eb52646
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-11-05 21:57:06 +0100

    Committing correct version of conflciting files

--- src/buttondevteam/perworld/main/MainPlugin.java
+++ src/buttondevteam/perworld/main/MainPlugin.java
@@ -1,61 +1,39 @@
 package buttondevteam.perworld.main;
 
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import buttondevteam.lib.TBMCCoreAPI;
 
 public class MainPlugin extends JavaPlugin {
-		
+	
+	
+	public static MainPlugin plugin;
+	public static volatile FileConfiguration config;
+	
+	public static volatile ConfigurationSection worlds;
+	public static volatile ConfigurationSection players;
+	public static long debugClock;
+	
+	
 	public void onEnable(){
 		
 		//getServer().getPluginManager().registerEvents(new WorldLoadListener(this), this);
 		TBMCCoreAPI.RegisterEventsForExceptions(new WorldChangeListener(this), this);
-		MainPlugin.getPlugin(this.getClass()).getServer().getListeningPluginChannels();
+		TBMCCoreAPI.RegisterEventsForExceptions(new ListenerPlayerWorldChange(this), this);
 		
 		saveDefaultConfig();
-		WorldChangeManager.init(this);
-		//BukkitTask task = new initWorldSettings().runTaskLater(this, 20);
-	}
-}
-/*	
-	public void initWorldSettings(){
-		Bukkit.getServer().getWorlds()
-				.stream()
-				.forEach(s -> {
-						FileConfiguration config = getConfig();
-						String name = s.getName();
-						
-						if (config.get(name + ".settings.shareinvgroup") == null)
-							config.set(name + ".settings.shareinvgroup", name);
-						
-						if (config.get(name + ".settings.sharedatagroup") == null)
-							config.set(name + ".settings.sharedatagroup", name);
-						
-						});
+		
+		plugin = this;
+		config = getConfig();
+		worlds = config.getConfigurationSection("worlds");
+		players = config.getConfigurationSection("players");
+		
+		if (!config.contains("worlds")) config.createSection("worlds");
+		if (!config.contains("players")) config.createSection("players");
 		saveConfig();
+		
+		new buttondevteam.perworld.cache.world.ShareSettings().initCache();
 	}
 }
-	
-	private void createConfig(){
-		try {
-			
-			
-			if (!getDataFolder().exists())
-				getDataFolder().mkdirs();
-		
-			
-			File file = new File(getDataFolder(), "config.yml");
-		
-			if (!file.exists()){
-				getLogger().info("Config.yml not found, creating!");
-				saveDefaultConfig();
-			}else
-				getLogger().info("Config.yml found, loading!");
-		
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-*/
